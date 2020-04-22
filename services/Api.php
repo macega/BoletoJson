92e507e94e68294fda19e14c871c5e4e55f458bf
Juliano Alves Medina <20805373+macega@users.noreply.github.com>
2020-04-22 13:55:01 -0400

    criado tratamento para retorno 401 do servidor rest

--- services/Api.php
+++ services/Api.php
@@ -31,19 +31,16 @@ class Api {
     public static function getJson($url) {
 
         $curl = curl_init($url);
-
         $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,";
         $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
-        $header[] = "Cache-Control: max-age=0";
+        //$header[] = "Cache-Control: max-age=0";
         $header[] = "Connection: keep-alive";
         $header[] = "Keep-Alive: 600";
         $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
-
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_TIMEOUT, 22);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 77);
-
         $response = curl_exec($curl);
         $info = curl_getinfo($curl);
         curl_close($curl);
